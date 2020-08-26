2a13e92b6acc82a59d9e9a8c3e7e77214bb92391
Kartikay Sarswat <49266512+KARTIKAY-SARSWAT@users.noreply.github.com>
2020-08-26 19:12:31 +0530

    right
    toggle button line  shifted below spacer class such that button is shifted to right

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,11 +8,11 @@ import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 const Navbar = props => (
         <header className="bar">
             <nav className="bar__navigation">
+                <div className="bar_logo"><a href="/">KS</a></div>
+                <div className="spacer" />
                 <div class="toolbar__toggle-button">
                     <DrawerToggleButton click={props.drawerClickHandler} />
                 </div>
-                <div className="bar_logo"><a href="/">SL</a></div>
-                <div className="spacer" />
                 <div className="bar_navigation-items">
                     <ul>
                         <li><a href="/">Home</a></li>
